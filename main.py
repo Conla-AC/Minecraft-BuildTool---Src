cd1be0e0df2377bee71797bd4a185d96b6b57fc2
Conla <3358048037@qq.com>
2026-06-19 19:21:52 +0800

    1
    1

---
+++ main.py
@@ -0,0 +1,12 @@
+import random
+b=0
+def main():
+  a=input("请输入你的导关次数。>")
+  if 122 > ord(a) > 97 and  65 < ord(a) <90:
+    while 1:
+      if random.randint(int(a),int(a)**random.randint(int(a))) == random.randint(int(a),int(a)**114):
+        print("导关失败。你几把炸了。")
+        break
+      print(f"成功导关 {b}")
+      b+=1
+  
